d3fdaec6eaf03dd5bca357c2551e4c8a4f6f7057
Thomas Clarke <Thomas_Clarke@live.com>
2019-06-26 14:54:22 +0100

    Update files

--- 2_Alternative_pipeline/sc3_script.R
+++ 2_Alternative_pipeline/sc3_script.R
@@ -1,7 +1,7 @@
 library(SC3)
 library(scater)
 library(SingleCellExperiment)
-
+library(zinbwave)
 
 test_data_gene_count_matrix <- read.delim("/home/tsc21/Documents/BS7120/Group_project/2_Alternative_pipeline/Full_data_gene_count_matrix.csv", row.names = 1)
 
@@ -11,7 +11,6 @@ info_file <- read.delim("/home/tsc21/Documents/BS7120/Group_project/SraRunTable(
 
 head(info_file)
 
-yan
 sce <- SingleCellExperiment(
   assays = list(
     counts = as.matrix(test_data_gene_count_matrix),
@@ -20,6 +19,9 @@ sce <- SingleCellExperiment(
   colData = info_file
 )
 
+
+sce_zinb <- zinbwave(sce, k=2, epsilon=22085)
+?zinbwave
 
 # define feature names in feature_symbol column
 rowData(sce)$feature_symbol <- rownames(sce)
@@ -30,7 +32,26 @@ sce <- sce[!duplicated(rowData(sce)$feature_symbol), ]
 plotPCA(sce, colour_by = "cell_type")
 
 
-sce <- sc3(sce, ks = 10, biology = TRUE)
+sce <- sc3(sce, ks = 2:10, biology = TRUE)
 
 sc3_interactive(sce)
+
+sc3_export_results_xls(sce)
+
+
+
+
+col_data <- colData(sce)
+head(col_data[ , grep("sc3_", colnames(col_data))])
+
+
+plotPCA(
+  sce, 
+  colour_by = "sc3_3_clusters", 
+  size_by = "sc3_3_log2_outlier_score"
+)
+
+
+
+
 
