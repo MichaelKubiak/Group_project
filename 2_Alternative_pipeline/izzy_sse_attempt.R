a7b6126c193c4369256f29ec04ba46103ad139b0
Izzy Russell <Russellizzyj@gmail.com>
2019-06-27 12:24:38 +0100

    sorry thomas

--- 2_Alternative_pipeline/izzy_sse_attempt.R
+++ 2_Alternative_pipeline/izzy_sse_attempt.R
@@ -6,12 +6,15 @@ library(SC3)
 
 
 # Load in data (set wd to project directory first)
-gene_expression_matrix <- read.delim("./2_Alternative_pipeline/Full_data_gene_count_matrix.csv", row.names = 1)
+gene_expression_matrix <- read.delim("./combined_data.txt", row.names = 1, header = TRUE)
 info_file <- read.delim("./2_Alternative_pipeline/Full_Experiment_metadata.txt")
 
+gene_expression_matrix <- data.frame(gene_expression_matrix)
+# start=0, end=466
 # Create SummarizedExperiment for Zinbwave input
-
-
+makeSummarizedExperimentFromDataFrame(df = gene_expression_matrix, start.field=1:1, end.field=22088:466,ignore.strand=FALSE,
+                                      seqnames.field=c("seqnames", "seqname", "chromosome", "chrom", "chr", "chromosome_name", "seqid"),
+                                      starts.in.df.are.0based = TRUE)
 # Zinbwave zero accounting
 
 # Take Zinbwave SingleCellExperiment output to SC3
