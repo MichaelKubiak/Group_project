af67f9dce01d2c5912ba1b06d0a9ef90939d9eb9
Thomas Clarke <Thomas_Clarke@live.com>
2019-06-28 16:22:31 +0100

    updated

--- 2_Alternative_pipeline/toms_R_script.R
+++ 2_Alternative_pipeline/toms_R_script.R
@@ -1,19 +1,25 @@
 # Aiming for a full analysis from start to finish on this script
 library(SummarizedExperiment)
 library(SingleCellExperiment)
-library(scater)
 library(SC3)
+library(zinbwave)
 
 
-# Load in data (set wd to project directory first)
 gene_expression_matrix <- read.delim("/home/tsc21/Documents/BS7120/Group_project/combined_data.txt", row.names = 1, header = TRUE)
 info_file <- read.delim("/home/tsc21/Documents/BS7120/Group_project/2_Alternative_pipeline/SraRunTable_all.txt")
 
 truncated_expression_matrix <- gene_expression_matrix[1:22085,]
+# start=0, end=466
 
-head(info_file)
+# Remove all 0 expressed genes from the dataset:
+truncated_expression_matrix <- truncated_expression_matrix[rowSums(truncated_expression_matrix[, -1])>0, ]
+truncated_expression_matrix
+# Create SummarizedExperiment for Zinbwave input
+#sum_exp <- makeSummarizedExperimentFromDataFrame(df = gene_expression_matrix, start.field=1:1, end.field=22088:466,ignore.strand=FALSE,
+#starts.in.df.are.0based = TRUE)
 
-sce <- SingleCellExperiment(
+# Go straight to making a summarisedexperiment?
+sum_exp <- SummarizedExperiment(
   assays = list(
     counts = as.matrix(truncated_expression_matrix),
     logcounts = log2(as.matrix(truncated_expression_matrix) + 1)
@@ -21,21 +27,22 @@ sce <- SingleCellExperiment(
   colData = info_file
 )
 
-
-# Scater section
-
-sce <- calculateQCMetrics(sce)
-colnames(colData(sce))
+# printing the summarisedexperiment data
+sum_exp
 
 
-colnames(rowData(sce))
 
 
-plotHighestExprs(sce, exprs_values = "counts")
 
-plotExprsFreqVsMean(sce)
+# Zinbwave zero accounting:
+# This is a flexible zero-inflated negative binomial model for low dimensional representations of single-cell RNA Seq
+# data. 
+# Zimbwave takes a SummarizedExperiment and returns a SingleCellExperiment object
+# k=how many latent variables we want to infer from the data, epsilon=num_genes
+library(BiocParallel)
+sce <- zinbwave(sum_exp, K=10, epsilon=21625, normalizedValues=TRUE, BPPARAM=MulticoreParam(4))
 
-plotRowData(sce, x = "n_cells_by_counts", y = "mean_counts")
+
 
 
 
@@ -48,18 +55,21 @@ rowData(norm_sce)$feature_symbol <- rownames(norm_sce)
 # remove features with duplicated names
 norm_sce <- norm_sce[!duplicated(rowData(norm_sce)$feature_symbol), ]
 
-
+# plot PCA
 plotPCA(norm_sce, colour_by = "cell_type")
 
 
+#sc3 analysis
 norm_sce <- sc3(norm_sce, ks = 5:15, biology = TRUE)
 
+#visualise sc3 analysis
 sc3_interactive(norm_sce)
 
+#create an xls file 
 sc3_export_results_xls(norm_sce)
 
 sc3_consensus(norm_sce)
-sc3_plot_consensus(sce, k = 2:10)
+sc3_plot_consensus(sce, k = 5:15)
 
 
 
@@ -68,6 +78,6 @@ head(col_data[ , grep("sc3_", colnames(col_data))])
 
 plotPCA(
   norm_sce, 
-  colour_by = "sc3_3_clusters", 
-  size_by = "sc3_3_log2_outlier_score"
+  colour_by = "cell_type", 
+  size_by = "cell_type"
 )
