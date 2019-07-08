c2ddd11922502724b13a1b1ec7233651cafd11c4
Thomas Clarke <Thomas_Clarke@live.com>
2019-07-08 13:41:03 +0100

    new_barcode_file

--- scPipe/Test/barcoder.py
+++ scPipe/Test/barcoder.py
@@ -1,29 +1,40 @@
 #comma deliniated with a header
 
 import gzip
-import re
+import shutil
+import os
+
+files = os.listdir("/home/tsc21/Documents/BS7120/Group_project/scPipe/Test")
 
 
-
-with gzip.open("SRR1974543_2.fastq.gz", "rb") as f:
-    file_content = f.readlines()
+for entities in files:
+    if entities.endswith(".gz"):
+        with gzip.open(entities, "rb") as f:
+            file_content = f.readlines()
 
 #with open("SRR1974543_1.fastq") as g:
 #    something = g.readlines()
 
-fastq_list = []
+        fastq_list = []
 
-for lines in file_content:
-    lines = str(lines)
-    lines = lines.lstrip("b'")
-    lines = lines.rstrip("\n'")
-    lines += "\n"
-    if not (lines.startswith("@") or lines.startswith("+")):
-        string = "TRUE" + lines
-        fastq_list += string
-    else:
-        fastq_list += lines
+        for lines in file_content:
+            lines = str(lines)
+            lines = lines.lstrip("b'")
+            lines = lines.rstrip("\n'")
+            lines += "\n"
+            if not (lines.startswith("@") or lines.startswith("+")):
+                string = "TRUE" + lines
+                fastq_list += string
+            else:
+                fastq_list += lines
+
+        filename = entities.split(".")
+        filename = filename[0] + ".fastq"       
+
+        with open(filename,"w") as h:
+            h.writelines(fastq_list)
 
 
-with open("cell1.2.fastq","w") as h:
-    h.writelines(fastq_list)
+
+        with open(filename, 'rb') as f_in, gzip.open( + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
