82d65c1d2ceffc0f2dbbb2710355b0b1f7c373e0
Izzy Russell <Russellizzyj@gmail.com>
2019-07-08 14:22:36 +0100

    commented and added print statements to the barcoder script

---
+++ Scripts/barcoder.py
@@ -0,0 +1,42 @@
+#comma deliniated with a header
+
+import gzip
+import shutil
+import os
+
+files = os.listdir("/home/izzy_r/Group_project/Project_repo/Group_project/DATA_fastQ/Test_fastq")
+
+files_done = 0
+for entities in files:
+    if entities.endswith(".gz"):
+        with gzip.open(entities, "rb") as f:
+            file_content = f.readlines()
+
+        fastq_list = []
+        files_to_do = len(files)
+        print("starting file:", str(files_done + 1))
+        for lines in file_content:
+            lines = str(lines)
+            lines = lines.lstrip("b'")
+            lines = lines.rstrip("\n'")
+            lines += "\n"
+            if not (lines.startswith("@") or lines.startswith("+")):
+                string = "TRUE" + lines
+                fastq_list += string
+            else:
+                fastq_list += lines
+
+        filename = entities.split(".")
+        filename = filename[0] + ".fastq"
+
+        with open(filename,"w") as h:
+            h.writelines(fastq_list)
+
+
+
+        with open(filename, 'rb') as f_in, gzip.open(filename + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+        os.remove(filename)
+        files_done += 1
+        print("files:", files_done, "/", files_to_do)
+
