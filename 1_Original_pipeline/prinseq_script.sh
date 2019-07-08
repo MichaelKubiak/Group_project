f7c8a36dc70e7fd2e289d58d1875139ec953bb13
Roisin Kearins <rsk17@student.le.ac.uk>
2019-07-08 22:44:35 +0100

    updated bash script

--- 1_Original_pipeline/prinseq_script.sh
+++ 1_Original_pipeline/prinseq_script.sh
@@ -2,7 +2,7 @@
 #where the raw data files are located within a directory
 raw_data="/home/rsk17/Group_project/1_Original_pipeline/scpipe_testing/"
 #loops through files within the named directory
-for i in /home/rsk17/Group_project/1_Original_pipeline/scpipe_testing/*
+for i in ~/Group_project/1_Original_pipeline/scpipe_testing/*
 do
 #two run through both files within a folder
 #pair read suffixes
@@ -33,23 +33,37 @@ do
 	d="_good_out_R2_fastqc"
 	
 
-	python3 fadapa1.py $j$c
+	overp1=$(python3 fadapa1.py $j$c)
+	overp2=$(python3 fadapa_caps.py $j$d)
 
+	cutadapt $overp1 $overp2 -e 0.15 -m 30 -o $jcutR1.fastq -p $jcutR2.fastq -j 3 $j$a $j$b
+	#cutadapt $overp2 -e 0.15 -m 30 -o  
 
-	#overp1=python3 fadapa1.py $j$c #command not found but still does cutadapt ish
-	#python3 fadapa1.py $j$d
-	cutadapt -overrep_seq.txt -e 0.15 -m 30 -o trimmedR1$j.fastq $j$a
-#	cutadapt $overp2 -e 0.15 -m 30 -o trimmedR2$j.fastq $j$b
-# $j$c python3 fadapa1.py $j$c -e 0.15 -m 30 -
-	#python3 fadapa1.py $j$d
-#cutadapt needs to be input into prinseq and STAR
-#-e 0.15 -m 30
 
 #re trim for orphan reads
-#	prinseq++ -min_len 30 -fastq ... -fastq2 ... -threads 3 -out_name $j -out_bad /dev/null/ -out_bad2 /dev/null/ -out_single /dev/null/ -out_single2 /dev/null/
+	prinseq++ -min_len 30 -fastq $jcutR1.fastq -fastq2 $jcutR2.fastq -threads 3 -out_name retrimmed$j -out_bad /dev/null/ -out_bad2 /dev/null/ -out_single /dev/null/ -out_single2 /dev/null/
 
 #Trim Galore: removal  of  nextera  adapters  using  Trim  Galore(--stringency  1). 
-#STAR: options (-outFilterType BySJout \--outFilterMultimapNmax 20 \--alignSJoverhangMin 8 \--alignSJDBoverhangMin 1 \--outFilterMismatchNmax 999 \--outFilterMismatchNoverLmax 0.04 \--alignIntronMin 20 \--alignIntronMax 1000000 \--alignMatesGapMax  1000000 \--outSAMstrandField  intronMotif  ).  
+trim_galore retrimmed$j$a retrimmed$j$b --nextera --stringency 1 --paired
+#how to install STAR
+#'''wget https://github.com/alexdobin/STAR/archive/2.7.1a.tar.gz
+#tar -xzf 2.7.1a.tar.gz
+#cd STAR-2.7.1a'''
+#needs to be run through spectre due to not enough RAM space on the laptop
+#/home/r/rsk17/STAR-2.7.1a/bin/Linux_x86_64/STAR --runThreadN 6 --runMode genomeGenerate --genomeDir ~/HG19 --genomeFastaFiles ~/hg19/hg19.fa --sjdbGTFfile ~/hg19/hg.GTF --sjdbOverhang 74 --genomeSAsparseD 4
+
+#create an index for genome
+#STAR --runThreadN 3 --runMode genomeGenerate --genomeDir HG19 --genomeFastaFiles ~/Group_project/1_Original_pipeline/hg19/hg19.fa --sjdbGTFfile ~/Group_project/1_Original_pipeline/hg19/hg.GTF --sjdbOverhang 74 
+
+# spectre version:
+#STAR --runThreadN 6 --runMode genomeGenerate --genomeDir ~/HG19 --genomeFastaFiles ~/hg19/hg19.fa --sjdbGTFfile ~/hg19/hg.GTF --sjdbOverhang 74 --genomeSAsparseD 4
+
+#to make a GTF file. used https://genome.ucsc.edu/cgi-bin/hgTables table browser and change the assembly type to GRCh37/hg19 and output format to GTF
+#STAR: options 
+/home/rsk17/Group_project/1_Original_pipeline/STAR --readFilesIn retrimmed$j"_good_out_R1_val_1.fq" retrimmed$j"_good_out_R2_val_2.fq" --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax  1000000 --outSAMstrandField  intronMotif --runThreadN 3 --genomeDir HG19 
+#--outFileNamePrefix $j
+
+
 
 #aligned reads converted to counts for every gene using HTSeq :(-m  intersection-nonempty \-s no). 
 
