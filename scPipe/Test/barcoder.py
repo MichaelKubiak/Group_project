5bac6927ed9ac8dfd2c2b3f01c53a33d202354d0
Thomas Clarke <Thomas_Clarke@live.com>
2019-07-08 16:00:47 +0100

    Revert "new_barcode_file"
    This reverts commit c2ddd11922502724b13a1b1ec7233651cafd11c4.

--- scPipe/Test/barcoder.py
+++ scPipe/Test/barcoder.py
@@ -1,37 +1,39 @@
 #comma deliniated with a header
 
 import gzip
-import shutil
-import os
-
-files = os.listdir("/home/tsc21/Documents/BS7120/Group_project/scPipe/Test")
+import re
 
 
+
+<<<<<<< HEAD
 for entities in files:
     if entities.endswith(".gz"):
         with gzip.open(entities, "rb") as f:
             file_content = f.readlines()
             f.close()
+=======
+with gzip.open("SRR1974543_2.fastq.gz", "rb") as f:
+    file_content = f.readlines()
+>>>>>>> parent of c2ddd11... new_barcode_file
 
 #with open("SRR1974543_1.fastq") as g:
 #    something = g.readlines()
 
-        fastq_list = []
+fastq_list = []
 
-        for lines in file_content:
-            lines = str(lines)
-            lines = lines.lstrip("b'")
-            lines = lines.rstrip("\n'")
-            lines += "\n"
-            if not (lines.startswith("@") or lines.startswith("+")):
-                string = "TRUE" + lines
-                fastq_list += string
-            else:
-                fastq_list += lines
+for lines in file_content:
+    lines = str(lines)
+    lines = lines.lstrip("b'")
+    lines = lines.rstrip("\n'")
+    lines += "\n"
+    if not (lines.startswith("@") or lines.startswith("+")):
+        string = "TRUE" + lines
+        fastq_list += string
+    else:
+        fastq_list += lines
 
-        filename = entities.split(".")
-        filename = filename[0] + ".fastq"       
 
+<<<<<<< HEAD
         with open(filename,"w") as h:
             h.writelines(fastq_list)
             h.close()
@@ -40,3 +42,7 @@ for entities in files:
         with open(filename, 'rb') as f_in, gzip.open(filename + '.gz', 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
             f_out.close()
+=======
+with open("cell1.2.fastq","w") as h:
+    h.writelines(fastq_list)
+>>>>>>> parent of c2ddd11... new_barcode_file
