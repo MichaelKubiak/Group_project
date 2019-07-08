41117577d09c78acfb1362fe0901238354736297
Izzy Russell <Russellizzyj@gmail.com>
2019-07-08 10:58:04 +0100

    scpipe multicell script functional

--- Scripts/scPipe_multicells.py
+++ Scripts/scPipe_multicells.py
@@ -1,13 +1,25 @@
 """
-scpipe must be executed 466 times, one for each cell
-This script will execute the scPipe R script for each pair of reads into a named directory
+Execute scpipe R script multiple times for a list of accession numbers
+This script will execute the scPipe R script for each pair of reads into a named directory.
+Outputs from the script appear where your working directory is.
+Before use configure the path for R_script to match your path, as well as any relevant paths in the R script.
 """
 import os
 import sys
 import subprocess
 import argparse
 
-SRR_acc_filepath = "/home/izzy_r/Group_project/Project_repo/Group_project/Scripts/SRR_testing.txt"
+# Argparse for user SRA accession list input
+parser = argparse.ArgumentParser(description='Combine cell data info into the header of the gene expression matrix')
+parser.add_argument("SRR_acc_list", help='input a text file containing SRR accessions separated by newlines (this can '
+                                         'be downloaded from the SRA website when obtaining data')
+args = parser.parse_args()
+
+# Link the R script the code is designed to run here
+R_script = "/home/izzy_r/Group_project/Project_repo/Group_project/2_Alternative_pipeline/scPipe/multicell_testing.R"
+# Collect the accession numbers the FASTQ data is downloaded from
+SRR_acc_filepath = args.SRR_acc_list
+
 if os.path.isfile(SRR_acc_filepath):
     print("SRR accession file located.")
 
@@ -15,23 +27,48 @@ else:
     sys.exit("ERROR: no SRR accession file located. Check the path and try again.")
 
 with open(SRR_acc_filepath) as SRR_accs:
-    SRR_acc_list = SRR_accs.readlines()
+    input_lines = SRR_accs.readlines()
 
-# for accession in SRR_acc_list:
-#     accession = accession.replace("\n", "")
-#     if os.path.exists(accession):
-#         print("using available directory", accession)
-#     else:
-#         os.mkdir(accession)
+# Removing newlines
+SRR_acc_list = []
+for line in input_lines:
+    if len(line) > 1:
+        SRR_acc = line.rstrip("\n")
+        SRR_acc_list.append(SRR_acc)
 
-R_script = "/home/izzy_r/Group_project/Project_repo/Group_project/2_Alternative_pipeline/scPipe/multicell_testing.R"
-file1 = "/home/izzy_r/Group_project/Project_repo/Group_project/DATA_fastQ/Test_fastq/SRR1974698_1.fastq"
-file2 = "/home/izzy_r/Group_project/Project_repo/Group_project/DATA_fastQ/Test_fastq/SRR1974698_2.fastq"
-SRR_1 = SRR_acc_list[0].strip("\n")
+# Check for broken accessions.
+count = 0
+miscount = 0
+for acc in SRR_acc_list:
+    if len(acc) != 10 and not acc.startswith("SRR"):
+        miscount += 0
+        print("erroneous SRR detected:", acc)
+    else:
+        count += 1
+print("SRR file appears to contain", str(count), "accessions. Beginning scPipe analysis")
 
-# example subprocess with arguments
-subprocess.run(["Rscript", R_script, "--SRR", SRR_1])
 
+# Running the R script:
+total_accessions = len(SRR_acc_list)
+print(total_accessions)
+acc_number = 1
 
+# Create SRR named directory where necessary
+for accession in SRR_acc_list:
+    remaining_accessions = total_accessions - acc_number
+    print("Beginning analysis for:", accession)
 
-# scPipe.R --file1 = SRRXXXXXXX_1.fastq.gz --file2 = SRRXXXXXXX_2.fastq.gz
+    if os.path.exists(accession):
+        print("using available directory", accession)
+    else:
+        os.mkdir(accession)
+    # Move to the accession directory
+    os.chdir("./" + accession)
+    # Run the R script with accession number for prefixes (See multicell_testing.R for further information)
+    subprocess.run(["Rscript", R_script, "--SRR", accession])
+    os.remove(accession + "combined_fastq.gz")
+    print("Completed", acc_number, "of", total_accessions, ". remaining accessions:", remaining_accessions)
+    os.chdir("../")
+    acc_number += 1
+
+print("scPipe analysis completed.")
