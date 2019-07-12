a6f887db33b2d1f6a333afda63407091d28b5698
Izzy Russell <Russellizzyj@gmail.com>
2019-07-12 09:21:23 +0100

    Rsubread multi implemented

---
+++ Scripts/Rsubread/Rsubread_multi.R
@@ -0,0 +1,146 @@
+#! /usr/bin/Rscript
+library(SingleCellExperiment)
+library(Rsubread)
+library(argparse)
+
+# Argparse
+parser <- ArgumentParser(description="takes fastq files inputs and output file prefix for scPipe iterations")
+parser$add_argument('--SRR', help='SRR prefix on input/output files')
+parser$add_argument('--build_index', help='Set to TRUE if an index has not previously been constructed or the reference have been changed', default = FALSE)
+
+arguments <- parser$parse_args()
+prefix <- arguments[1]
+rebuild_index <- arguments[2]
+data_dir = "."
+
+if (rebuild_index=='T'){
+  rebuild_index=TRUE
+}
+
+print(isTRUE(rebuild_index))
+
+# Reference file path:
+reference_fasta = file.path("/home/izzy_r/Group_project/Project_repo/Group_project/Reference_genome/ensembl/Homo_sapiens.GRCh38.dna.primary_assembly.fa")  
+reference_anno = file.path("/home/izzy_r/Group_project/Project_repo/Group_project/Reference_genome/ensembl/Homo_sapiens.GRCh38.97.chr.gff3") 
+
+# breakdown file names into constituent parts for file renaming
+fastq_file1 = paste(prefix, "_1.fastq.gz", sep = "")
+fq_R1 = file.path("/home/izzy_r/Group_project/Project_repo/Group_project/DATA_fastQ/Test_fastq/", fastq_file1) 
+
+fastq_file2 = paste(prefix, "_2.fastq.gz", sep = "")
+fq_R2 = file.path("/home/izzy_r/Group_project/Project_repo/Group_project/DATA_fastQ/Test_fastq/", fastq_file2)
+
+
+# Rsubread build/align to human reference genome if no index present
+if (rebuild_index == TRUE){
+  print("Building index from reference files")
+  Rsubread::buildindex(basename=file.path(data_dir, "read_index"), reference=reference_fasta, 
+                       gappedIndex = TRUE, indexSplit = TRUE)
+  index = file.path("/home/izzy_r/Group_project/Project_repo/Group_project/testing/", prefix)
+} else{
+  print("using available reference index:")
+  index = file.path("/home/izzy_r/Group_project/Project_repo/Group_project/testing/Index")
+}
+
+
+print("Rsubread align start")
+out.aln.bam <- paste(prefix, "out.aln.bam", sep = "")
+Rsubread::align(index=file.path(index, "read_index"),
+                readfile1=file.path(data_dir, combined_fastq.gz),
+                output_file=file.path(data_dir, out.aln.bam), 
+                type=0, nthreads=3 )
+print("Rsubread align end")
+
+# Assigning reads to annotated exons
+print("assign reads to exon start")
+out.map.bam <- paste(prefix, "out.map.bam", sep = "")
+out.map.bam
+sc_exon_mapping(file.path(data_dir, out.aln.bam),
+                file.path(data_dir, out.map.bam),
+                reference_anno, bc_len = 4, UMI_len = 0)
+print("assign reads to exon end")
+
+featurecounting <- Rsubread::featureCounts(files = file.path(data_dir, out.aln.bam),
+                                           
+                                           #annotation
+                                           annot.inbuilt = NULL,
+                                           annot.ext = reference_anno,
+                                           isGTFAnnotationFile = TRUE,
+                                           GTF.featureType = "exon",
+                                           GTF.attrType = "gene_name",
+                                           chrAliases = NULL,
+                                           
+                                           #level of summarisation
+                                           useMetaFeatures = TRUE,
+                                           
+                                           #overlap between reads and features
+                                           allowMultiOverlap = FALSE,
+                                           minOverlap = 1,
+                                           fracOverlap = 0,
+                                           fracOverlapFeature = 0,
+                                           largestOverlap = FALSE,
+                                           nonOverlap = 0,
+                                           nonOverlapFeature = NULL,
+                                           
+                                           #Read shifts, extension and reduction
+                                           readShiftType = "upstream",
+                                           readShiftSize = 0,
+                                           readExtension5 = 0,
+                                           readExtension3 = 0,
+                                           read2pos = 0,
+                                           
+                                           #multi-mapping reads
+                                           countMultiMappingReads = TRUE,
+                                           
+                                           #fractional counting
+                                           fraction = FALSE,
+                                           
+                                           #long reads with nanopore etc
+                                           isLongRead = FALSE,
+                                           
+                                           #read filtering
+                                           minMQS = 0,
+                                           splitOnly = FALSE,
+                                           nonSplitOnly = FALSE,
+                                           primaryOnly = FALSE,
+                                           ignoreDup = FALSE,
+                                           
+                                           #strandness
+                                           strandSpecific = 0,
+                                           
+                                           #exon-exon junctions
+                                           juncCounts = FALSE,
+                                           genome = reference_fasta,
+                                           
+                                           #Pair-end data parameters
+                                           isPairedEnd = TRUE,
+                                           requireBothEndsMapped = TRUE,
+                                           
+                                           checkFragLength = FALSE,
+                                           minFragLength = 20,
+                                           maxFragLength = 300,
+                                           countChimericFragments = TRUE,
+                                           autosort = TRUE,
+                                           
+                                           #CPU thread count
+                                           nthreads = 3,
+                                           
+                                           #read group
+                                           byReadGroup = FALSE,
+                                           
+                                           #Report assignment result for each read
+                                           reportReads = "CORE",
+                                           #Null = save report file in working directory
+                                           reportReadsPath = NULL,
+                                           
+                                           #miscellaneous
+                                           maxMOp =10,
+                                           tmpDir=".",
+                                           verbose = TRUE
+)
+
+# Write the output to a file:
+gene_counting.csv <- paste(prefix, "gene_counting.csv", sep = "")
+write.table(featurecounting[["counts"]],file = gene_counting.csv, col.names = NA, sep = "\t")
+
+
---
+++ Scripts/Rsubread/Rsubread_multi.py
@@ -0,0 +1,118 @@
+"""
+Execute scpipe R script multiple times for a list of accession numbers
+This script will execute the scPipe R script for each pair of reads into a named directory.
+Outputs from the script appear where your working directory is.
+Before use configure the path for R_script to match your path, as well as any relevant paths in the R script.
+"""
+import os
+import sys
+import subprocess
+import argparse
+
+def move_index():
+    os.rename("read_index.00.b.array", "../Index/read_index.00.b.array")
+    os.rename("read_index.00.b.tab", "../Index/read_index.00.b.tab")
+    os.rename("read_index.files", "../Index/read_index.files")
+    os.rename("read_index.log", "../Index/read_index.log")
+    os.rename("read_index.reads", "../Index/read_index.reads")
+
+
+# Argparse for user SRA accession list input
+parser = argparse.ArgumentParser(description='Combine cell data info into the header of the gene expression matrix')
+parser.add_argument("SRR_acc_list", help='input a text file containing SRR accessions separated by newlines (this can '
+                                         'be downloaded from the SRA website when obtaining data')
+parser.add_argument('--index', help='set to True to rebuild an index if it has not been built or the reference '
+                                    'genome has been changed', default=False)
+args = parser.parse_args()
+
+# User input for building the reference index True/ False
+rebuild_index = args.index
+
+# Link the R script the code is designed to run here
+R_script = "/home/izzy_r/Group_project/Project_repo/Group_project/Scripts/Rsubread/Rsubread_multi.R"
+
+# Collect the accession numbers the FASTQ data is downloaded from
+SRR_acc_filepath = args.SRR_acc_list
+
+# Find and process the accession list provided by the user
+if os.path.isfile(SRR_acc_filepath):
+    print("SRR accession file located.")
+else:
+    sys.exit("ERROR: no SRR accession file located. Check the path and try again.")
+with open(SRR_acc_filepath) as SRR_accs:
+    input_lines = SRR_accs.readlines()
+
+# Removing newlines
+SRR_acc_list = []
+for line in input_lines:
+    if len(line) > 1:
+        SRR_acc = line.rstrip("\n")
+        SRR_acc_list.append(SRR_acc)
+
+# Check for broken accessions.
+count = 0
+miscount = 0
+bad_accs = []
+for acc in SRR_acc_list:
+    if len(acc) != 10 and not acc.startswith("SRR"):
+        miscount += 0
+        bad_accs.append(acc)
+    else:
+        count += 1
+if miscount > 0:
+    print("Warning. Erroneous accessions detected.")
+    for acc in bad_accs:
+        print(acc)
+print("SRR file appears to contain", str(count), "accessions. Beginning scPipe analysis")
+
+# Running the R script:
+
+# totals for progress messages
+total_accessions = len(SRR_acc_list)
+acc_number = 1
+
+# Create SRR named directory where necessary
+for accession in SRR_acc_list:
+    # Stop subsequent inputs from building index if first run requires it
+    if acc_number > 1:
+        rebuild_index = False
+
+    # Create R boolean strings:
+    if rebuild_index:
+        Rindex = "T"
+    else:
+        Rindex = "F"
+
+    # Progress statement
+    remaining_accessions = total_accessions - acc_number
+    print("Beginning analysis for:", accession, "Accession number: ", str(acc_number))
+
+    # Create/check for SRR directory for each cell analysis
+    if os.path.exists(accession):
+        print("using available directory", accession)
+    else:
+        os.mkdir(accession)
+
+    # Move to the accession directory
+    os.chdir("./" + accession)
+
+    # Run the R script with accession number for prefixes (See multicell_testing.R for further information)
+    subprocess.run(["Rscript", R_script, "--SRR", accession, "--build_index", Rindex])
+
+    # Remove large files that are not required to save disk space
+    os.remove(accession + "out.map.bam")
+    os.remove(accession + "out.aln.bam")
+
+    # Progress statement
+    print("Completed", acc_number, "of", total_accessions, ". remaining accessions:", remaining_accessions)
+
+    # Move the index files if new index built so the rest of the accessions can easily access the files separately
+    if rebuild_index and acc_number == 1:
+        move_index()
+
+    # Reset the working directory for the next loop
+    os.chdir("../")
+    acc_number += 1
+
+# Final 
+print("scPipe analysis completed.")
