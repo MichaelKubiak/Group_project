76c20a7c97e07de76642ff8199ebda6edde55e9d
Izzy Russell <Russellizzyj@gmail.com>
2019-07-16 16:04:05 +0100

    commented

--- Scripts/sql_count_matrix.py
+++ Scripts/sql_count_matrix.py
@@ -6,10 +6,11 @@ parser.add_argument("input", help='input a tab delineated gene expression matrix
 parser.add_argument('--output', help='manually set output path', default=".")
 args = parser.parse_args()
 
-#
+# Read in the input gene count matrix
 with open(args.input) as matrix:
     data = matrix.readlines()
 
+# Process the first row which contains all the cell names
 cell_names = data[0].rstrip("\n").split("\t")
 cell_IDs = []
 for name in cell_names:
@@ -17,24 +18,16 @@ for name in cell_names:
         name = name[:10]
         cell_IDs.append(name)
 
-gene_IDs = []
-expression_data_rows = []
-for line in data[1:]:
-    line = line.split("\t")
-    gene_IDs.append(line[0])
-    expression_data_rows.append(line[1:])
-
-output = "sql_expression_table"
-
-expr = ""
-
+# process and write each expression data point from the remaining rows alongside gene and cell information
 for line in data[1:]:
     line = line.strip("\n")
     line = line.split("\t")
     gene = line[0]
     numbers = line[1:]
-    print(numbers[464])
+    print("working.....", numbers[464]) # print statement with variable end number so user can see visual progress
     for i in range(len(numbers)):
         if int(numbers[i]) != 0:
             with open(args.output, "a+") as out:
                 out.write(gene + "\t" + cell_IDs[i] + "\t" + numbers[i] + "\n")
+
+print("File creation completed.")
