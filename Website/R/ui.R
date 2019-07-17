71acf025561890cc767d9f67152660b1349a9a92
Michael Kubiak <michael.kubiak@bath.edu>
2019-07-17 13:35:30 +0100

    more shiny

--- Website/R/ui.R
+++ Website/R/ui.R
@@ -2,16 +2,21 @@ library(shiny)
 library(plotly)
 library(colourpicker)
 
+
 load("./pca_data.RData")
+
 fluidPage(
-  titlePanel("PCA plot")
+  titlePanel("PCA plot"),
   sidebarLayout(
     sidebarPanel(
-      colourInput()
+      colourInput("colad", "Select colour for adult neurons", value="blue"),
+      colourInput("colfq", "Select colour for foetal quiescent neurons", value="red"),
+      colourInput("colfr", "Select colour for foetal replicating neurons", value="orange")
+    ),
+    mainPanel(
+      plotlyOutput("plot1"),
+      verbatimTextOutput("Hi")
     )
   )
-  mainPanel(
-    plotlyOutput("plot1"),
-    verbatimTextOutput("Hi")
-  )
+  
 )
