2c41c8df7082a36670aa95a005e5b078c52a52dd
Michael Kubiak <michael.kubiak@bath.edu>
2019-07-23 17:00:14 +0100

    improved dropdown

--- Website/java_script/functions.js
+++ Website/java_script/functions.js
@@ -1,16 +1,8 @@
 function p1Dropdown(){
-    document.getElementById("Graphs").classList.toggle("show");
+    document.getElementById("Graphs").classList.add("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-} 
+function closeMenu(){
+        document.getElementById("Graphs").classList.remove("show");
+}
+
