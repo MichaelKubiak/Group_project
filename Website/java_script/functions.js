2916b96be647a148431810f82f51dc833f99e246
Michael Kubiak <michael.kubiak@bath.edu>
2019-07-24 15:59:20 +0100

    spanning trees sorted

--- Website/java_script/functions.js
+++ Website/java_script/functions.js
@@ -1,8 +1,19 @@
-function p1Dropdown(){
+var inner = "none";
+function gDropdown(){
     document.getElementById("Graphs").classList.add("show");
+    document.getElementById("Spanning").style.display = inner;
 }
 
-function closeMenu(){
-        document.getElementById("Graphs").classList.remove("show");
+function gcloseMenu(){
+    document.getElementById("Graphs").classList.remove("show");
+    inner = "none";
+}
+
+function sDropdown(){
+    inner = "block";
+}
+
+function scloseMenu(){
+    inner = "none";
 }
 
