ed82057a1abccbba89337c223094b14f1dae78ea
Michael Kubiak <michael.kubiak@bath.edu>
2019-07-26 13:39:52 +0100

    original pipeline finished on website

--- Website/java_script/functions.js
+++ Website/java_script/functions.js
@@ -1,12 +1,15 @@
 var inner = "none";
+var clust = "none";
 function gDropdownOG(){
     document.getElementById("Graphs").classList.add("show");
     document.getElementById("Spanning").style.display = inner;
+    document.getElementById("Clustering").style.display = clust;
 }
 
 function gcloseMenuOG(){
     document.getElementById("Graphs").classList.remove("show");
     inner = "none";
+    clust = "none";
 }
 
 function sDropdownOG(){
@@ -15,6 +18,14 @@ function sDropdownOG(){
 
 function scloseMenuOG(){
     inner = "none";
+}
+
+function cDropdownOG(){
+    clust = "block";
+}
+
+function ccloseMenuOG(){
+    clust = "none";
 }
 
 function gDropdownNew(){
