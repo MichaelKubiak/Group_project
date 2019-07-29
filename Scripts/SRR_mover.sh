1feea57ecbc286c5aee194aae6c886e3de0997cb
Izzy Russell <Russellizzyj@gmail.com>
2019-07-29 11:27:43 +0100

    additional comments

--- Scripts/SRR_mover.sh
+++ Scripts/SRR_mover.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# copy files 
+# copy a certain number of files to a new location (used for splitting data for data processing)
 input="/home/izzy_r/Group_project/Project_repo/Group_project/Scripts/SRR_1_acc_list.txt"
 while IFS= read -r line
 do
