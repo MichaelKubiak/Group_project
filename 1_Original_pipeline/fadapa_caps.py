607740a0a1e8af22974e7fa8c4184b7667ea7de5
Roisin Kearins <rsk17@student.le.ac.uk>
2019-07-29 11:02:59 +0100

    newly commented for appendix

--- 1_Original_pipeline/fadapa_caps.py
+++ 1_Original_pipeline/fadapa_caps.py
@@ -1,26 +1,31 @@
+#searching for overrepresented sequences within a genome sequence and producing an output for the extraction of these sequences in a upper case format as the sequences can be in lower or upper case. It is used instead of FASTQC when FASTQC cannot find any sequences. This can then be used by cutadapt
+
+
 from fadapa import Fadapa
 import sys
-
+#input
 file_one = sys.argv[1]
 
-#f = Fadapa('/home/rsk17/Group_project/1_Original_pipeline/SRR1974725_1_fastqc/fastqc_data.txt')
-#f2 = Fadapa('/home/rsk17/Group_project/1_Original_pipeline/SRR1974725_2_fastqc/fastqc_data.txt')
 f = Fadapa('/home/rsk17/Group_project/1_Original_pipeline/'+file_one+'/fastqc_data.txt')
-
+#look for certain phrases ie Overrepresented sequences 
 good_seq = f.raw_data("Overrepresented sequences")[0]
-#good_seq2 = f2.raw_data("Overrepresented sequences")[0]
+#create an empty list fo the overrepresented sequences to be appended to 
 seq_list = []
-#seq_list2 = [] 
+
 
 #if good_seq is pass in this case use ==
-if good_seq != ">>Overrepresented sequences	pass":
+#if there is no overrepresented sequences pass then the loop continues, otherwise the loop is broken
+#find when Overrepresented sequences are not a pass (!=)
+if good_seq != ">>Overrepresented sequences     pass":
 	for data in f.clean_data('Overrepresented sequences'):
 		seq_list.append(data[0])
 
 
-#this gets the output into the correct format for cutadapt (-a XXXX etc)
+#this gets the output into the correct format for cutadapt (-A XXXX etc)
+#creates output and uses the seq_list from before to output the found sequences into a format that can be used by cutadapt.
 output = ""
+#seq_list[1:] means that everything that isnt seq_list[0] (#sequence)
 for items in seq_list[1:]:
 	output = output + "-A" + items
-#+ ">" + file_one + "\n" + "-a " + items + "\n"
+
 print(output)
